aaee0e0c7dd923b7eb02037cb0a4e847c8702750
Cedric Dujardin <cedric.dujardin@laposte.net>
2019-12-21 18:21:51 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -8,10 +8,12 @@ class Dog
     @name
   end
 
-
-
-  def initialize.breed=(breed)
+  def initialize(breed)
     @breed = "Mutt"
+  end
+
+  def breed=(breed)
+    @breed = breed
   end
 
   def breed
